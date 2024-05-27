0e59531e08d464964a8cd0e13b2fd1129e3bb646
kevinyjna <77737775+kevinyjna@users.noreply.github.com>
2024-05-28 00:50:39 +0900

    Time: 28 ms (75.09%), Space: 11.8 MB (11.99%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
@@ -5,13 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        #O(n^2) solution
-        n = len(nums)
-        count = 0
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] < target:
-                    count += 1
-        return count
+        # #Nested loop: Time complexity O(n^2) solution
+        # n = len(nums)
+        # count = 0
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         if nums[i] + nums[j] < target:
+        #             count += 1
+        # return count
         
+        #Two pointer: Time complexity O(n*log n)
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += (right - left)
+                left += 1
+            else:
+                right -= 1
+        return count
         
