4474f77480c4a5298f4610386709083c444644eb
kevinyjna <77737775+kevinyjna@users.noreply.github.com>
2024-10-17 23:01:18 +0900

    Time: 37 ms (52.26%), Space: 16.7 MB (5.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,26 +1,22 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        
-        
-        if len(str1) < len(str2):
-            shorter, longer = str1, str2
+        if str1 > str2:
+            longer, shorter = str1, str2
         else:
-            shorter, longer = str2, str1
-        
-        def isDivisible(baseStr, inputStr):
-            if (len(inputStr) % len(baseStr) != 0) :
+            shorter, longer = str1, str2
+            
+        def isDivisible(base, inputStr):
+            k = len(inputStr) // len(base)
+            if len(inputStr) % len(base) != 0:
                 return False
-            repeatCount = len(inputStr) // len(baseStr)
-            return repeatCount * baseStr == inputStr
-
+            return base * k == inputStr
         base = shorter
-        while (len(base) > 0) :
-            if isDivisible(base, shorter) and isDivisible(base, longer):
+        for i in range(len(base)):
+            if isDivisible(base, str1) and isDivisible(base, str2):
                 return base
             else:
                 base = base[:-1]
-        
-         
-        return ""
-        
                 
+        return ""
+            
+        
